95bafa61bf49d59cb1c414c5b4428f194937a570
Peter Li <lihaohong6@gmail.com>
2025-11-18 02:20:13 -0500

    parse character tags and apply them to infoboxes

--- character_info/char_infobox.py
+++ character_info/char_infobox.py
@@ -1,7 +1,35 @@
+from dataclasses import dataclass
+from functools import cache
+
 from wikitextparser import parse
 
 from character_info.characters import get_characters, get_character_pages
-from utils.wiki_utils import find_template_by_name, save_page
+from utils.data_utils import autoload
+from utils.wiki_utils import find_template_by_name, save_page, set_arg
+
+
+@dataclass
+class CharacterTag:
+    id: int
+    name: str
+    type: int
+
+
+@cache
+def get_character_tag_dict() -> dict[int, CharacterTag]:
+    data = autoload("CharacterTag")
+    result: dict[int, CharacterTag] = dict()
+    for k, v in data.items():
+        result[int(k)] = CharacterTag(v['Id'], v['Title'], v['TagType'])
+    return result
+
+
+def get_character_tags(char_id: int) -> list[CharacterTag]:
+    data = autoload("CharacterDes")
+    tag_dict = get_character_tag_dict()
+    return [tag_dict[tag_id]
+            for tag_id in data[str(char_id)]['Tag']]
+
 
 def update_infobox():
     auto_link = ["Lucky Oasis"]
@@ -26,7 +54,11 @@ def update_infobox():
         for arg, value in pairs:
             for link in auto_link:
                 value = value.replace(link, f"[[{link}]]")
-            target.set_arg(f" {arg} ", f" {value}\n")
+            set_arg(target, arg, value)
+        tags = get_character_tags(char.id)
+        set_arg(target, "role", tags[0].name)
+        set_arg(target, "style", tags[1].name)
+        set_arg(target, "faction", tags[2].name)
         save_page(page, str(parsed), "update infobox")
 
 
