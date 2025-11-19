5fc10213395cb24232a824d0155286442865b01e
Peter Li <lihaohong6@gmail.com>
2025-11-18 22:05:23 -0500

    Automate some character image uploads

---
+++ character_info/char_images.py
@@ -0,0 +1,25 @@
+from character_info.characters import get_characters
+from utils.data_utils import assets_root
+from utils.upload_utils import UploadRequest, process_uploads
+
+
+def upload_char_images():
+    chars = get_characters()
+    req = []
+    for char in chars.values():
+        head_root = assets_root / "icon" / "head"
+        path = head_root / f"head_{char.id}01_xxl.png"
+        page = f"{char.name}-head-xxl.png"
+        req.append(UploadRequest(path, page, ""))
+        path = head_root / f"head_{char.id}01_xl.png"
+        page = f"{char.name}.png"
+        req.append(UploadRequest(path, page, ""))
+    process_uploads(req)
+
+
+def main():
+    upload_char_images()
+
+
+if __name__ == "__main__":
+    main()
