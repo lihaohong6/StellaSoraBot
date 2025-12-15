3578babc04f5c547ce5c2b15fe84c244afe9bf4f
Peter Li <lihaohong6@gmail.com>
2025-12-14 20:23:01 -0500

    Audio: export easter egg lines not in archive

--- character_info/audio.py
+++ character_info/audio.py
@@ -1,6 +1,8 @@
 import subprocess
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import cache
+from itertools import groupby
 from pathlib import Path
 
 from pywikibot import Page
@@ -20,8 +22,8 @@ class AudioLine:
     transcription_jp: str
     transcription_cn: str
     translation: str
-    sort_key: int
     voice_type: int
+    sort_key: int = -1
 
     def file_name(self, lang: str):
         return f"{self.source}_{lang}"
@@ -43,58 +45,122 @@ def process_transcription(original: list[str] | str) -> str:
 
 
 @cache
+def get_transcriptions(source: str) -> tuple[str, str, str] | None:
+    bubble_data_jp: dict = load_json_from_path(jp_root / "bubble/_jp/BubbleData.json")
+    if source not in bubble_data_jp:
+        return None
+    bubble_data_cn: dict = load_json_from_path(cn_root / "bubble/_cn/BubbleData.json")
+    bubble_data_en: dict = load_json_from_path(en_root / "bubble/_en/BubbleData.json")
+    transcription_jp = process_transcription(bubble_data_jp[source]['text']['female_jp'])
+    transcription_cn = process_transcription(bubble_data_cn[source]['text']['female_cn'])
+    translation = process_transcription(bubble_data_en[source]['text']['female_jp'])
+    return transcription_jp, transcription_cn, translation
+
+
+voice_title_mapping = {
+    'valentine': "Valentine's day",
+    'xmas': "Christmas",
+    'newyear': "New Year",
+    'yostar': "Title screen",
+    "yourbirth": "Player birthday",
+    "birth": "Trekker birthday",
+}
+
+
+def append_vo_directory_data(result: dict[int, list[AudioLine]]):
+    existing_sources: set[str] = set()
+    for _, lst in result.items():
+        for line in lst:
+            existing_sources.add(line.source)
+    data = autoload("VoDirectory")
+    vo_types: set[str] = set()
+    for k, v in data.items():
+        voice_id = int(k)
+        source = v['voResource']
+        if source in existing_sources:
+            continue
+        char_id = v['characterId']
+        if char_id not in result:
+            continue
+        voice_type = v['votype']
+        title = voice_title_mapping[voice_type]
+        transcription_jp, transcription_cn, translation = get_transcriptions(source)
+        result[char_id].append(AudioLine(
+            id=voice_id,
+            title=title,
+            source=source,
+            transcription_jp=transcription_jp,
+            transcription_cn=transcription_cn,
+            translation=translation,
+            voice_type=3,
+            sort_key=list(voice_title_mapping.keys()).index(voice_type)
+        ))
+
+
+@cache
 def get_audio() -> dict[int, list[AudioLine]]:
-    bubble_data_jp = load_json_from_path(jp_root / "bubble/_jp/BubbleData.json")
-    bubble_data_cn = load_json_from_path(cn_root / "bubble/_cn/BubbleData.json")
-    bubble_data_en = load_json_from_path(en_root / "bubble/_en/BubbleData.json")
-    data = autoload("CharacterArchiveVoice")
+    result = get_voice_archive_data()
+    append_vo_directory_data(result)
+    return result
+
+
+def get_voice_archive_data() -> dict[int, list[AudioLine]]:
     result: dict[int, list[AudioLine]] = {}
+    data = autoload("CharacterArchiveVoice")
     for k, v in data.items():
         char_id = v['CharacterId']
         if char_id not in result:
             result[char_id] = []
         voice_type = v['ArchVoiceType']
-        source = v['Source']
-        sort_key = v['Sort']
-        s1, s2 = source.split("_")
-        s2 = int(s2)
-        type_strings = ['combat', 'ui']
-        if voice_type == 1:
-            type_strings.reverse()
-        type_strings.append('')
-        # posterchat 7 corresponds to discuss 1, and so on
-        if s1 == "posterchat" and s2 >= 7:
-            s1 = "discuss"
-            s2 = s2 - 6
-        sources = [
-            f"vo_{char_id}_{type_string}_{s1}_{s2:03d}"
-            for type_string in type_strings
-        ]
-        if sort_key >= 76:
-            s2 = sort_key - 75
-            sources.append(
-                f"vo_story_{char_id}_{s2:03d}_{s1}"
-            )
-        sources.extend([source.lower()
-                        for source in sources
-                        if source.lower() != source])
-        for source in sources:
-            if source in bubble_data_en:
-                break
-        else:
-            print(f"Audio: {source} not found")
+        source = determine_audio_source(v, char_id, voice_type)
+        if source is None:
             continue
+        tr_jp, tr_cn, translation = get_transcriptions(source)
         result[char_id].append(AudioLine(
             id=int(k),
             title=v['Title'],
             source=source,
-            transcription_jp=process_transcription(bubble_data_jp[source]['text']['female_jp']),
-            transcription_cn=process_transcription(bubble_data_cn[source]['text']['female_cn']),
-            translation=process_transcription(bubble_data_en[source]['text']['female_jp']),
+            transcription_jp=tr_jp,
+            transcription_cn=tr_cn,
+            translation=translation,
             sort_key=v['Sort'],
             voice_type=voice_type,
         ))
     return result
+
+
+def determine_audio_source(v: dict, char_id: int, voice_type: int) -> str | None:
+    source = v['Source']
+    sort_key = v['Sort']
+    s1, s2 = source.split("_")
+    s2 = int(s2)
+    type_strings = ['combat', 'ui']
+    if voice_type == 1:
+        type_strings.reverse()
+    type_strings.append('')
+    # posterchat 7 corresponds to discuss 1, and so on
+    if s1 == "posterchat" and s2 >= 7:
+        s1 = "discuss"
+        s2 = s2 - 6
+    sources = [
+        f"vo_{char_id}_{type_string}_{s1}_{s2:03d}"
+        for type_string in type_strings
+    ]
+    if sort_key >= 76:
+        s2 = sort_key - 75
+        sources.append(
+            f"vo_story_{char_id}_{s2:03d}_{s1}"
+        )
+    sources.extend([source.lower()
+                    for source in sources
+                    if source.lower() != source])
+    for source in sources:
+        if get_transcriptions(source) is not None:
+            break
+    else:
+        print(f"Audio: {source} not found")
+        return None
+    return source
 
 
 def wav_to_ogg(wav_path: Path, ogg_path: Path) -> Path:
@@ -108,17 +174,19 @@ def wav_to_ogg(wav_path: Path, ogg_path: Path) -> Path:
 
 def upload_audio_files(char_name: str, audio_lines: list[AudioLine]) -> None:
     upload_requests = []
+    ogg_dir = audio_wav_root / 'ogg'
+    ogg_dir.mkdir(parents=True, exist_ok=True)
     for audio_line in audio_lines:
         for lang in ["jp", "cn"]:
             filename = f"{audio_line.source}_{lang}"
             source = audio_wav_root / f"{filename}.wav"
             if not source.exists():
                 continue
-            temp = temp_dir / f"{filename}.ogg"
-            wav_to_ogg(source, temp)
+            ogg_file = ogg_dir / f"{filename}.ogg"
+            wav_to_ogg(source, ogg_file)
             target_page = audio_line.file_page(lang)
             upload_requests.append(UploadRequest(
-                source=temp,
+                source=ogg_file,
                 target=target_page,
                 text=f"[[Category:{char_name} voice lines]]",
                 summary="upload voice lines"
@@ -153,14 +221,20 @@ def generate_audio_page():
             continue
         lines = audio[char.id]
         upload_audio_files(char.name, lines)
+        lines_by_type = defaultdict(list)
+        for line in lines:
+            lines_by_type[line.voice_type].append(line)
         result = ["{{TrekkerAudioTop}}",
                   "",
                   "==Daily Voice Chat==",
-                  lines_to_template([l for l in lines if l.voice_type == 1]),
+                  lines_to_template(lines_by_type[1]),
                   "",
                   "==Combat Voice Chat==",
-                  lines_to_template([l for l in lines if l.voice_type == 2]),
+                  lines_to_template(lines_by_type[2]),
                   "",
+                  "==Special Voice Lines==",
+                  lines_to_template(lines_by_type[3]),
+                  ""
                   "{{TrekkerAudioBottom}}"]
         save_page(page, "\n".join(result), summary="update voice lines page")
 
