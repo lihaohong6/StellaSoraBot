c5d627958d789697b921cd53b0abd4dbcd47ee2f
Peter Li <lihaohong6@gmail.com>
2026-01-06 03:08:12 -0500

    Add CG uploader

---
+++ page_generators/cg_uploader.py
@@ -0,0 +1,32 @@
+from utils.data_utils import assets_root
+from utils.upload_utils import UploadRequest, process_uploads
+
+
+def upload_cgs():
+    p = assets_root / "imageavg" / "avgcg"
+    images = p.glob("*.png")
+    image_categories = [
+        ("story_event", "Event story CGs"),
+        ("story_main", "Main story CGs"),
+        ("story_tales", "Side story CGs"),
+    ]
+    upload_requests = []
+    for f in images:
+        for search_keyword, category in image_categories:
+            if search_keyword not in f.name:
+                continue
+            upload_requests.append(UploadRequest(
+                f,
+                "File:" + f.name,
+                f"[[Category:{category}]]",
+                "batch upload cutscenes"))
+            break
+    process_uploads(upload_requests)
+
+
+def main():
+    upload_cgs()
+
+
+if __name__ == "__main__":
+    main()
