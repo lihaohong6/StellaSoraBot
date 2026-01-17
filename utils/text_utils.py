bd16275f9adac491b63e51ebb54da0caac89df1c
Peter Li <lihaohong6@gmail.com>
2026-01-17 06:19:44 -0500

    Improve utf-8 text unescaping in escape_text

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -2,11 +2,21 @@ import re
 
 
 def escape_text(text: str) -> str:
-    text = text.replace("==RT==", "\n").replace("\n", "<br/>").replace("==PLAYER_NAME==", "<username>")
+    text = (text
+            .replace("==RT==", "\n")
+            .replace("\n", "<br/>")
+            .replace("==PLAYER_NAME==", "<username>")
+            .replace("==W==", ""))
     text = re.subn("~~(?=~)", "~~<nowiki/>", text)[0]
-    text = (text.replace(r"\226\128\148", "—")
-            .replace(r"\226\128\166", "…")
-            .replace(r"\226\128\153", "’"))
+    def repl(m):
+        b1, b2, b3 = (int(x) for x in m.groups())
+        return bytes((b1, b2, b3)).decode('utf-8')
+
+    text = re.subn(
+        r'\\(\d{1,3})\\(\d{1,3})\\(\d{1,3})',
+        repl,
+        text
+    )[0]
     text = text.replace("=", "{{=}}")
     return text
 
